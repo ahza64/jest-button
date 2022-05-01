605287f7f27a9fd03b6d18812c02605a6111164c
ahza64 <justinmichaelpettit@gmail.com>
2022-05-01 13:27:12 -0700

    first functional test with built funtional components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from 'react'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red')
+  const newbuttonColor = buttonColor === 'red' ? 'blue' : 'red'
+
   return (
     <div>
-      <button style={{backgroundColor: 'red'}}>Change to blue</button>
+      <button style={{backgroundColor: buttonColor}} onClick={() => setButtonColor(newbuttonColor)}>Change to {newbuttonColor}</button>
     </div>
   );
 }
